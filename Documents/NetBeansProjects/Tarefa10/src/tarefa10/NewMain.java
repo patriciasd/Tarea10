19286ddf248f74b6c3a201f3c14829365fb9119e
Patricia SD <patrisuarezdiaz@gmail.com>
2022-03-17 18:16:12 +0100

    Segundo commit commit

---
+++ Documents/NetBeansProjects/Tarefa10/src/tarefa10/NewMain.java
@@ -0,0 +1,8 @@
+package tarefa10;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+       System.out.println(Restador.restar (15f,4f));
+    }
+}
---
+++ Documents/NetBeansProjects/Tarefa10/src/tarefa10/Restador.java
@@ -0,0 +1,8 @@
+package tarefa10;
+
+public class Restador {
+
+    public static float restar(float a, float b) {
+        return a - b;
+    }
+}
